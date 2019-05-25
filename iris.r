409b73faaa38f9b49b202df5845096a84df4dcd6
sam1610 <samir.amri@gmail.com>
2019-05-25 23:00:26 +0300

    update factors

--- iris.r
+++ iris.r
@@ -7,3 +7,10 @@ head(iris)
     filter( Sepal.Width>3.6)
     iris<- iris(as.na())
 qplot(data=iris , Sepal.Width, Sepal.Length )
+summary(mtcars)
+
+factor(mtcars$gear)
+
+f <- factor(c(3.4, 1.2, 5))
+as.numeric(f)
+levels(f)
